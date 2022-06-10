d4ad72d13550c260dfaa1f74d4f7fdcca6bd4858
jreynard <jeremy.reynard@cosmotech.com>
2022-06-10 12:24:51 +0200

    Add volume in run.sh to retrieve exported files

--- run.sh
+++ run.sh
@@ -9,5 +9,6 @@ docker run \
 -e TWIN_CACHE_NAME="$(printenv TWIN_CACHE_NAME)" \
 -e TWIN_CACHE_PORT="$(printenv TWIN_CACHE_PORT)" \
 -e EXPORT_PATH="$(printenv EXPORT_PATH)" \
+-v "$(printenv EXPORT_PATH)":"$(printenv EXPORT_PATH)" \
 -e LOG_LEVEL="INFO" \
 twincache-connector
