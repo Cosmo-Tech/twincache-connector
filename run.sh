2786dae3e20f0f7b00d974cfafead4e29b5b8912
jreynard <jeremy.reynard@cosmotech.com>
2022-07-04 13:04:06 +0200

    Update CosmoTech_Acceleration_Library version to 0.1.7

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ docker run \
 -e TWIN_CACHE_HOST="$(printenv TWIN_CACHE_HOST)" \
 -e TWIN_CACHE_NAME="$(printenv TWIN_CACHE_NAME)" \
 -e TWIN_CACHE_PORT="$(printenv TWIN_CACHE_PORT)" \
+-e TWIN_CACHE_PASSWORD="$(printenv TWIN_CACHE_PASSWORD)" \
 -e CSM_FETCH_ABSOLUTE_PATH="$(printenv CSM_FETCH_ABSOLUTE_PATH)" \
 -v "$(printenv CSM_FETCH_ABSOLUTE_PATH)":"$(printenv CSM_FETCH_ABSOLUTE_PATH)" \
 -e LOG_LEVEL="INFO" \
